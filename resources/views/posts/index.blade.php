c2f6ba154a4e92c6ee1cb327b92d357922cec887
HamTart <hamtart@gmail.com>
2020-12-15 11:54:11 +0000

    Posts view and controller added

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title','Posts')
+
+@section('content')
+
+    <p>All posts</p>
+    <ul>
+        @foreach ($posts as $post)
+            <li> <a href = "{{ route('posts.show', ['post' => $post->id]) }}">{{$post->title}}</li>
+        @endforeach
+    </ul>
+
+
+@endsection
