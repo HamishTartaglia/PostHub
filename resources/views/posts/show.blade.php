cedd7eca321b7bfe6d215e00bddda9fdf4f550ce
HamTart <hamtart@gmail.com>
2020-12-15 14:28:02 +0000

    Tags are now shown for each post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,15 @@
         <li>Score: {{$post->score}}</li>
         <li>By: <a href = "{{ route('profiles.show', ['profile' => $post->profile->id]) }}"> {{$post->profile->username}} </a> </li>
         <li>From: <a href = "{{ route('categories.show', ['category' => $post->category->id]) }}"> {{$post->category->name}} </a> </li>
+        <li>Tags:
+            <ul>
+                @foreach ($post->tags as $tag)
 
+                    <li>{{$tag->name}} </a> </li>
+     
+                @endforeach
+            </ul>
+            
     </ul>
 
 
