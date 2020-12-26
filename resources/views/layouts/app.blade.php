f9bcf82aff137df82988155b4d8c9be839853eb7
HamTart <hamtart@gmail.com>
2020-12-26 15:45:20 +0000

    Home button is now the same colour as the favicon - blue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand mb-0 h1" href="{{ url('/') }}">PostHub</a>
+                <a class="navbar-brand mb-0 h1" href="{{ url('/') }}" style="color:#0065bd">PostHub</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
