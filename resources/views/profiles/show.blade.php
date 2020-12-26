ea1bd2f664b3d42c6dde96cfa6ffd0c501e1fbda
HamTart <hamtart@gmail.com>
2020-12-26 16:47:14 +0000

    Everything on profile.show is now in a 'container' div

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -4,47 +4,51 @@
 
 @section('content')
 
-    <p>Bio: {{$profile->bio}}</p>
+    <div class="container">
 
-    @can('update', $profile)
-        <button><a href="{{ route('profile.edit', ['profile' => $profile]) }}"> Edit</a></button>
-    @endcan
+        <p>Bio: {{$profile->bio}}</p>
 
-    <p>Score: {{$profile->score}}</p>
-    <p>Account created: {{ $profile->created_at->diffForHumans() }} </p>
-    <p>Posts:</p>
+        @can('update', $profile)
+            <button><a href="{{ route('profile.edit', ['profile' => $profile]) }}"> Edit</a></button>
+        @endcan
 
-    <ul>
-        @foreach ($profile->posts as $post)
+        <p>Score: {{$profile->score}}</p>
+        <p>Account created: {{ $profile->created_at->diffForHumans() }} </p>
+        <p>Posts:</p>
 
-            <li> <a href = "{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}"> {{$post->title}} </a> </li>
-         
-        @endforeach
+        <ul>
+            @foreach ($profile->posts as $post)
 
-    </ul>
-
-    <p>Posted in:<p>
-    
-    <ul>
-        @foreach ($profile->posts->unique('category_id') as $post)
-
-            <li><a href = "{{ route('categories.show', ['category' => $post->category]) }}"> {{$post->category->name}} </a> </li>
+                <li> <a href = "{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}"> {{$post->title}} </a> </li>
             
-        @endforeach
+            @endforeach
 
-    </ul>
+        </ul>
 
-    @can('update', $profile)
-        <a href="{{ route('logout') }}">Logout</a>
-    @endcan
+        <p>Posted in:<p>
+        
+        <ul>
+            @foreach ($profile->posts->unique('category_id') as $post)
 
-    @can('delete', $profile)
-        <form action="{{ route('profile.destroy', ['profile' => $profile]) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit">Delete Profile</button>
-        </form>
-    @endcan
+                <li><a href = "{{ route('categories.show', ['category' => $post->category]) }}"> {{$post->category->name}} </a> </li>
+                
+            @endforeach
+
+        </ul>
+
+        @can('update', $profile)
+            <a href="{{ route('logout') }}">Logout</a>
+        @endcan
+
+        @can('delete', $profile)
+            <form action="{{ route('profile.destroy', ['profile' => $profile]) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit">Delete Profile</button>
+            </form>
+        @endcan
+
+    </div>
     
 
 @endsection
