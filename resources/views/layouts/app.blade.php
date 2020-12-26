92c2d08d011a10b5cf48a54f08b56e2a90b75453
HamTart <hamtart@gmail.com>
2020-12-26 14:07:23 +0000

    Formatted the navbar so it looks good now

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.21.1/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-    
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -26,19 +26,19 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">PostHub</a>
+                <a class="navbar-brand mb-0 h1" href="{{ url('/') }}">PostHub</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li><a class="navbar-brand" href="{{ route('categories.show', ['category' => 'Animals' ]) }}"> Animals </a></li>
-                        <li><a class="navbar-brand" href="{{ route('categories.show', ['category' => 'Funny' ]) }}"> Funny </a></li>
-                        <li><a class="navbar-brand" href="{{ route('categories.show', ['category' => 'Gaming' ]) }}"> Gaming </a></li>
-                        <li><a class="navbar-brand" href="{{ route('categories.show', ['category' => 'AskAnything' ]) }}"> AskAnything </a></li>
-                        <li><a class="navbar-brand" href="{{ route('categories.show', ['category' => 'Misc' ]) }}"> Misc </a></li>
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item"><a class="nav-link {{ request()->is('categories/Animals') ? 'active' : '' }}" href="{{ route('categories.show', ['category' => 'Animals' ]) }}"> Animals </a></li>
+                        <li class="nav-item"><a class="nav-link {{ request()->is('categories/Funny') ? 'active' : '' }}" href="{{ route('categories.show', ['category' => 'Funny' ]) }}"> Funny </a></li>
+                        <li class="nav-item"><a class="nav-link {{ request()->is('categories/Gaming') ? 'active' : '' }}" href="{{ route('categories.show', ['category' => 'Gaming' ]) }}"> Gaming </a></li>
+                        <li class="nav-item"><a class="nav-link {{ request()->is('categories/AskAnything') ? 'active' : '' }}" href="{{ route('categories.show', ['category' => 'AskAnything' ]) }}"> AskAnything </a></li>
+                        <li class="nav-item"><a class="nav-link {{ request()->is('categories/Misc') ? 'active' : '' }}" href="{{ route('categories.show', ['category' => 'Misc' ]) }}"> Misc </a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -54,24 +54,7 @@
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{route('profiles.show', ['profile' => Auth::user()->profile])}}" 
-                                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->profile->username }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                            <li class="nav-item"><a class="navbar-brand" href="{{route('profiles.show', ['profile' => Auth::user()->profile])}}"> {{ Auth::user()->profile->username }} </a></li>
                         @endguest
                     </ul>
                 </div>
