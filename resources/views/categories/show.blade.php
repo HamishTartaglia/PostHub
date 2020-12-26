edba3d2aacb3d1c1f76ef441d45cfee3046ea48e
HamTart <hamtart@gmail.com>
2020-12-26 14:18:55 +0000

    Post index page and category show pages now dont show pullet point for each post

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -9,7 +9,7 @@
                 @foreach ($posts as $post)
                     <div>
 
-                        <li> 
+                        <li class="list-unstyled"> 
                             <a href = "{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}"> {{$post->title}} </a> <br>
                             <a> {{$post->body}} </a><br>
                             <a href = "{{ route('profiles.show', ['profile' => $post->profile->username]) }}"> {{$post->profile->username}} </a>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,13 +5,12 @@
 @section('content')
 
     <div class="container">
-        <p>All posts</p>
-            <ul>
-                @foreach ($posts as $post)
-                    <li> <a href = "{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}">{{$post->title}}</li>
-                @endforeach
-            </ul>
-            {{$posts->links()}}
+        <ul class="list-unstyled">
+            @foreach ($posts as $post)
+                <li> <a href = "{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}">{{$post->title}}</li>
+            @endforeach
+        </ul>
+        {{$posts->links()}}
     </div>
     
 
