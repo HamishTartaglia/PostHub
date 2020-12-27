59a6dacff3ab4fa1954d4b2dc90d8b46bee47858
HamTart <hamtart@gmail.com>
2020-12-27 14:30:02 +0000

    added padding to categories.show to match with posts.index

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -4,33 +4,33 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="container pt-3 px-4">
         <div class="d-flex justify-content-end">
             <a href = "{{ route('post.create', ['category' => $category])}}"><button type="button" class="btn">Create Post</button></a>
         </div>
         <br>
         <ul>
-                @foreach ($posts as $post)
-                    <div class="post" onclick="location.href='{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}'">
+            @foreach ($posts as $post)
+                <div class="post" onclick="location.href='{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}'">
 
-                        <li class="list-unstyled"> 
-                            <h6>{{$post->title}}</h6>
-                            <p class="text-muted">{{ Illuminate\Support\Str::limit($post->body, $limit = 100, $end = '...') }}</p>
-                            <div class="navbar">
-                                <p class="postedBy">Posted by:
-                                    <a href = "{{ route('profiles.show', ['profile' => $post->profile->username]) }}" class="user"> {{$post->profile->username}} </a></p>
-                                <p class="posted"> Posted: {{ $post->created_at->diffForHumans() }} </p>
-                            </div>
-                        </li>
+                    <li class="list-unstyled"> 
+                        <h6> {{$post->title}} </h6>
+                        <p class="text-muted">{{ Illuminate\Support\Str::limit($post->body, $limit = 100, $end = '...') }}</p>
+                        <div class="navbar">
+                            <p class="postedBy">Posted by:
+                                <a href = "{{ route('profiles.show', ['profile' => $post->profile->username]) }}" class="user"> {{$post->profile->username}} </a></p>
+                            <p class="posted"> Posted: {{ $post->created_at->diffForHumans() }} </p>
+                        </div>
+                    </li>
 
-            
-                    </div><br>
-                @endforeach
-            </ul>
+        
+                </div><br>
+            @endforeach
+        </ul>
 
-            <div class="d-flex justify-content-center">
-                {{$posts->links()}}
-            </div>
+        <div class="d-flex justify-content-center">
+            {{$posts->links()}}
+        </div>
     </div>
     
 
