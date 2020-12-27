b9bb3de549dbd55e10180f26463f0670c5997671
HamTart <hamtart@gmail.com>
2020-12-27 14:40:18 +0000

    profile.edit styled to look like the other edit pages

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -4,14 +4,24 @@
     
 @section('content')
 
-    <form method="POST" action="{{ route('profile.update', ['profile' => $profile])}}">
-        @csrf
-        @method('PUT')
-        <p>Bio: <input type="text" name="bio"
-            value="{{$profile->bio}}"> </p>
 
-        <input type="submit" value="Submit">
-        <a href = "{{ route('profiles.show', ['profile' => $profile])}}">Cancel</a>
-    </form>
+    <div class="d-flex justify-content-center">
+        <form method="POST" action="{{ route('profile.update', ['profile' => $profile])}}" id="edit">
+            @csrf
+            @method('PUT')
+            <div class="edit-title">
+                <h5>Edit your profile - {{$profile->username}}</h5>
+            </div><br>
+            
+            <h5>Bio: </h5>
+            <input type="text" name="bio" value="{{$profile->bio}}" class="form-control">
+            <br>
+            <div class="navbar">
+                <a href = "{{ route('profiles.show', ['profile' => $profile])}}"><button type="button" class="btn">Cancel</button></a>
+                <input type="submit" value="Submit" class="btn">
+            </div>
+        </form>
+    </div>
+
     
 @endsection
