5a84f1f31509d197904a154795eca2e783b7fbc8
HamTart <hamtart@gmail.com>
2020-12-27 12:52:01 +0000

    Styled the text areas on the post.create page

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,17 +5,17 @@
 @section('content')
 
     <div class="d-flex justify-content-center">
-        <form method="POST" action="{{ route('post.store', $category->id)}}">
+        <form method="POST" action="{{ route('post.store', $category->id)}}" id="create">
             @csrf
             <div>
                 <div class="create-title">
                     <h5>Create a post in {{$category->name}}</h5>
                 </div><br>
                 <h5>Title: </h5>
-                <textarea type="text" name="title" value="{{ old('title') }} " class="form-control"></textarea>
-
+                <input type="text" name="title" value="{{ old('title') }} " class="form-control">
+                <br>
                 <h5>Body: </h5>
-                <textarea type="text" name="body" value="{{ old('body') }} " class="form-control"></textarea>
+                <textarea type="text" name="body" value="{{ old('body') }} " class="form-control" id="post-body"></textarea>
             </div>
             {{--
             <p>Category ID: <input type="text" name="category_id"
