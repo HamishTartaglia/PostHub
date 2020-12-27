5ee8574bbe3647191733d9e1d0566a3fa7d2d940
HamTart <hamtart@gmail.com>
2020-12-27 14:25:08 +0000

    Styled posts.index to fit with other pages

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,14 +4,34 @@
 
 @section('content')
 
-    <div class="container">
-        <ul class="list-unstyled">
+    <div class="container p-4">
+        <div class="d-flex justify-content-center" id="posts-title">
+            <h5>All Posts</h5>
+        </div>
+        <br>
+        <ul>
             @foreach ($posts as $post)
-                <li> <a href = "{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}">{{$post->title}}</li>
+                <div class="post" onclick="location.href='{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}'">
+
+                    <li class="list-unstyled"> 
+                        <h6> {{$post->title}} </h6>
+                        <p class="text-muted">{{ Illuminate\Support\Str::limit($post->body, $limit = 100, $end = '...') }}</p>
+                        <div class="navbar">
+                            <p class="postedBy">Posted by:
+                                <a href = "{{ route('profiles.show', ['profile' => $post->profile->username]) }}" class="user"> {{$post->profile->username}} </a></p>
+                            <p class="posted">In: <a href = "{{ route('categories.show', ['category' => $post->category->name]) }}" class="user"> {{$post->category->name}}</a></p>
+                            <p class="posted"> Posted: {{ $post->created_at->diffForHumans() }} </p>
+                        </div>
+                    </li>
+
+        
+                </div><br>
             @endforeach
         </ul>
-        {{$posts->links()}}
+
+            <div class="d-flex justify-content-center">
+                {{$posts->links()}}
+            </div>
     </div>
-    
 
 @endsection
