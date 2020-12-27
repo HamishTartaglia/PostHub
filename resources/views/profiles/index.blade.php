900822f2ca1ca6cf59cfa9275434cd9f378386e6
HamTart <hamtart@gmail.com>
2020-12-27 14:10:40 +0000

    Profiles.index is now styled the same as categories.index

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,12 +4,16 @@
 
 @section('content')
 
-    <p>All profiles</p>
-    <ul>
+    <div class="container p-4" id="profiles">
+        <div class="d-flex justify-content-center" id="profiles-title">
+            <h5>All profiles</h5>
+        </div>
+        <br>
         @foreach ($profiles as $profile)
-
-            <li> <a href = "{{ route('profiles.show', ['profile' => $profile]) }}"> {{$profile->username}}</li>
-                
+            <div class="d-flex justify-content-center" id="profile" onclick="location.href='{{ route('profiles.show', ['profile' => $profile]) }}'">
+                <h6>{{$profile->username}}</h6>
+            </div><br>
         @endforeach
-    </ul>
+    </div>
+
 @endsection
