d166459e25f88627fc973533a4dc1537bd2fcc76
HamTart <hamtart@gmail.com>
2020-12-27 11:37:02 +0000

    Centered everything on the post create page

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,34 +3,37 @@
 @section('title','Create Post')
     
 @section('content')
-    <form method="POST" action="{{ route('post.store', $category->id)}}">
-        @csrf
-        <p>Title: <input type="text" name="title"
-            value="{{ old('title') }} "></p>
 
-        <p>Body: <input type="text" name="body"
-            value="{{ old('body') }} "></p>
+    <div class="d-flex justify-content-center">
+        <form method="POST" action="{{ route('post.store', $category->id)}}">
+            @csrf
+            <p>Title: <input type="text" name="title"
+                value="{{ old('title') }} "></p>
 
-        {{--
-        <p>Category ID: <input type="text" name="category_id"
-            value="{{ old('category_id') }} "></p>
-            
+            <p>Body: <input type="text" name="body"
+                value="{{ old('body') }} "></p>
 
-        <select name = "category_id">
-            @foreach ($categories as $category)
-
-                <option value="{{ $category->id }}"
-                    @if ($category->id == old('category_id'))
-                        selected="selected"
-                    @endif
-                    >
-                    {{ $category->name}}
-                </option>
+            {{--
+            <p>Category ID: <input type="text" name="category_id"
+                value="{{ old('category_id') }} "></p>
                 
-            @endforeach
-            --}}
 
-        <input type="submit" value="Submit">
-        <a href = "{{ route('categories.show', ['category' => $category ])}}">Cancel</a>
-    </form>
+            <select name = "category_id">
+                @foreach ($categories as $category)
+
+                    <option value="{{ $category->id }}"
+                        @if ($category->id == old('category_id'))
+                            selected="selected"
+                        @endif
+                        >
+                        {{ $category->name}}
+                    </option>
+                    
+                @endforeach
+                --}}
+
+            <input type="submit" value="Submit">
+            <a href = "{{ route('categories.show', ['category' => $category ])}}">Cancel</a>
+        </form>
+    </div>
 @endsection
