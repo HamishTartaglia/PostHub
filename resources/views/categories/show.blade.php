fea9c72673712400ca3d904eb09f5f1d099cbf94
HamTart <hamtart@gmail.com>
2020-12-29 18:50:48 +0000

    Added a sidebar for the category.show page to list the admins of that page

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -3,41 +3,59 @@
 @section('content')
 
     <div class="container p-4">
-        <div>
-            <div class="d-flex justify-content-center" id="posts-title">
-                <h5>{{$category->name}}</h5>
+        <div class="d-flex" id="wrapper">
+
+        
+            <div class="border-right shadow p-3 mb-5 bg-white" id="admin-sidebar">
+                <h5> Admins: </h5>
+                <ul>
+                    @foreach ($category->admins as $admin)
+                        <p> <a href="{{route('profiles.show',['profile' => $admin->profile])}}" class="category-link"> {{$admin->profile->username}} </a></p>
+                    @endforeach
+                </ul>   
+
+
             </div>
-            <br>
 
-            @if (Auth::check())
-                <div class="d-flex justify-content-end">
-                    <a href = "{{ route('post.create', ['category' => $category])}}"><button type="button" class="btn">Create Post</button></a>
-                </div>
-            @endif
-        </div>
-        <br>
-        
-        <ul>
-            @foreach ($posts as $post)
-                <div class="post" onclick="location.href='{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}'">
 
-                    <li class="list-unstyled"> 
-                        <h6> {{$post->title}} </h6>
-                        <p class="text-muted">{{ Illuminate\Support\Str::limit($post->body, $limit = 100, $end = '...') }}</p>
-                        <div class="navbar">
-                            <p class="postedBy">Posted by:
-                                <a href = "{{ route('profiles.show', ['profile' => $post->profile->username]) }}" class="user"> {{$post->profile->username}} </a></p>
-                            <p class="posted"> Posted: {{ $post->created_at->diffForHumans() }} </p>
+            <div id="page-content-wrapper">
+                <div>
+                    <div class="d-flex justify-content-center" id="posts-title">
+                        <h5>{{$category->name}}</h5>
+                    </div>
+                    <br>
+
+                    @if (Auth::check())
+                        <div class="d-flex justify-content-end">
+                            <a href = "{{ route('post.create', ['category' => $category])}}"><button type="button" class="btn">Create Post</button></a>
                         </div>
-                    </li>
+                    @endif
+                </div>
+                <br>
+                
+                <ul>
+                    @foreach ($posts as $post)
+                        <div class="post" onclick="location.href='{{ route('posts.show', ['category' => $post->category,'post' => $post->id ]) }}'">
 
-        
-                </div><br>
-            @endforeach
-        </ul>
+                            <li class="list-unstyled"> 
+                                <h6> {{$post->title}} </h6>
+                                <p class="text-muted">{{ Illuminate\Support\Str::limit($post->body, $limit = 100, $end = '...') }}</p>
+                                <div class="navbar">
+                                    <p class="postedBy">Posted by:
+                                        <a href = "{{ route('profiles.show', ['profile' => $post->profile->username]) }}" class="user"> {{$post->profile->username}} </a></p>
+                                    <p class="posted"> Posted: {{ $post->created_at->diffForHumans() }} </p>
+                                </div>
+                            </li>
 
-        <div class="d-flex justify-content-center">
-            {{$posts->links()}}
+                
+                        </div><br>
+                    @endforeach
+                </ul>
+
+                <div class="d-flex justify-content-center">
+                    {{$posts->links()}}
+                </div>
+            </div>
         </div>
     </div>
 
