5b6be8f7189934633756967916dacf892394a98a
HamTart <hamtart@gmail.com>
2020-12-29 19:01:14 +0000

    Profile.show page shows if the user is an admin of each category

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -7,46 +7,59 @@
         <div class="d-flex" id="wrapper">
 
             <div class="border-right shadow p-3 mb-5 bg-white" id="sidebar-wrapper">
-                <div class="list-group list-group-flush">
-                    <div class="navbar">
-                        <h5> {{$profile->username}} </h5>
-                        @can('update', $profile)
-                            <a href="{{ route('profile.edit', ['profile' => $profile]) }}" class="edit-link">
-                                <h5><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                                 </svg></h5>
-                            </a>
-                        @endcan
-                    </div>
-                    
-                    <h6>{{$profile->bio}}</h6>
-                    <p>Score: {{$profile->score}}</p>
-                    <p>Account created: {{ $profile->created_at->diffForHumans() }} </p>
-                    @if (!$profile->posts->isEmpty())
-                        <p id="posted-in">Posted in:<p>
-                        <ul>
-                            @foreach ($profile->posts->unique('category_id') as $post)
-                                <a href = "{{ route('categories.show', ['category' => $post->category]) }}" class="category-link"> 
-                                    <p class="categories-posted">{{$post->category->name}} </p>
-                                </a>
-                            @endforeach
-                        </ul>
-                    @endif
-
-                    <div class="navbar">
-                        @can('update', $profile)
-                            <a href="{{ route('logout') }}"><button class="btn">Logout</button></a>
-                        @endcan
-
-                        @can('delete', $profile)
-                            <form action="{{ route('profile.destroy', ['profile' => $profile]) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn">Delete Profile</button>
-                            </form>
-                        @endcan
-                    </div>                    
+                <div class="navbar">
+                    <h5> {{$profile->username}} </h5>
+                    @can('update', $profile)
+                        <a href="{{ route('profile.edit', ['profile' => $profile]) }}" class="edit-link">
+                            <h5><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                </svg></h5>
+                        </a>
+                    @endcan
                 </div>
+                
+                <h6>{{$profile->bio}}</h6>
+                <p>Score: {{$profile->score}}</p>
+                <p>Account created: {{ $profile->created_at->diffForHumans() }} </p>
+                @if (!$profile->posts->isEmpty())
+                    <p id="posted-in">Posted in:<p>
+                    <ul>
+                        @foreach ($profile->posts->unique('category_id') as $post)
+                            <a href = "{{ route('categories.show', ['category' => $post->category]) }}" class="category-link"> 
+                                <p class="categories-posted">{{$post->category->name}} </p>
+                            </a>
+                        @endforeach
+                    </ul>
+                @endif
+
+                @php
+                    $admins = App\Admin::where('profile_id',$profile->id)->first()
+                @endphp
+
+                @if ($admins)
+                    <p id="posted-in">Admin of:<p>
+                    <ul>
+                        @foreach ($profile->admin->categories as $category)
+                            <a href = "{{ route('categories.show', ['category' => $category]) }}" class="category-link"> 
+                                <p class="admin-of">{{$category->name}} </p>
+                            </a>
+                        @endforeach
+                    </ul>                   
+                @endif
+
+                <div class="navbar">
+                    @can('update', $profile)
+                        <a href="{{ route('logout') }}"><button class="btn">Logout</button></a>
+                    @endcan
+
+                    @can('delete', $profile)
+                        <form action="{{ route('profile.destroy', ['profile' => $profile]) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn">Delete Profile</button>
+                        </form>
+                    @endcan
+                </div>                    
             </div>
 
             <div id="page-content-wrapper">
