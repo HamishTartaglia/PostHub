ad790ad141c22a90768570b1e51fb3cb023f793f
HamTart <hamtart@gmail.com>
2021-01-01 18:34:34 +0000

    Installed guzzle and performed a get request to nasa api using it and an api key

--- app/NasaPicture.php
+++ app/NasaPicture.php
@@ -11,5 +11,16 @@ class NasaPicture
     public function __construct($apiKey) 
     {
         $this->apiKey = $apiKey;
+        $this->getInfo();
+    }
+    
+    public function getInfo()
+    {
+        $client = new Client();
+
+        $response = $client->request('GET', 'https://api.nasa.gov/planetary/apod?api_key='.$this->apiKey);
+        $info = $response->getBody()->getContents();
+
+        $decode = json_decode($info);
     }
 }
