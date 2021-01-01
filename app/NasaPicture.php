b965a41f10d1c85ec7e858e585d4f27132b7fa52
HamTart <hamtart@gmail.com>
2021-01-01 18:39:01 +0000

    Added code to decode the json data retrieved from the api

--- app/NasaPicture.php
+++ app/NasaPicture.php
@@ -7,6 +7,11 @@ class NasaPicture
 {
 
     private $apiKey;
+    private $info;
+    private $photoUrl;
+    private $author;
+    private $description;
+    private $date;
 
     public function __construct($apiKey) 
     {
@@ -22,5 +27,17 @@ class NasaPicture
         $info = $response->getBody()->getContents();
 
         $decode = json_decode($info);
+        
+        $photoUrl = $decode->url;
+        $this->photoUrl = $photoUrl;
+
+        $author = $decode->copyright;
+        $this->author = $author;
+
+        $description = $decode->explanation;
+        $this->description = $description;
+
+        $date = $decode->date;
+        $this->date = $date;
     }
 }
