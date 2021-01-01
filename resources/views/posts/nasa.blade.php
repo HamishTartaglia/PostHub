09d140184ded2189ba6fc34bf300c931784fc383
HamTart <hamtart@gmail.com>
2021-01-01 19:27:22 +0000

    Added the nasa view - displays the picture of the day along with the details about it

---
+++ resources/views/posts/nasa.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container px-4">
+        <div class="row"id="posts-title">
+            <div class="col" id="post-title-col">
+                <h5>{{$title}}</h5>
+            </div>          
+        </div>
+
+        <br>
+
+        <div class="d-flex justify-content-center" id="post-body-text">
+            <h6>{{$description}}</h6>       
+        </div>
+        <div class="d-flex justify-content-center" id="post-pic">
+            <img src={{$photo}}>
+        </div>
+
+
+        <div class="row" id="post-info">
+            <div class="col">
+                <p class="postedBy">Taken by:
+                    {{$author}} 
+                </p>
+            </div>
+            <div class="col">
+            </div>
+            <div class="col">
+                <p class="posted">Posted: {{ $date }} </p>
+            </div>
+        </div>
+
+
+@endsection
