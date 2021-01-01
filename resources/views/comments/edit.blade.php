407f0099ed4d6f74bea34028b3f76a182bf5caff
HamTart <hamtart@gmail.com>
2021-01-01 15:07:13 +0000

    Updated the comment.edit page to match the style of the website

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -2,14 +2,28 @@
 
 @section('content')
 
-        <form method="POST" action="{{ route('comment.update', ['comment' => $comment])}}">
-        @csrf
-        @method('PUT')
-        <p>Body: <input type="text" name="body"
-            value="{{$comment->body}}"> </p>
+    <div class="d-flex justify-content-center">
+        <form method="POST" action="{{ route('comment.update', ['comment' => $comment])}}" id="edit">
+            @csrf
+            @method('PUT')
+            <div class="edit-title">
+                <h5>Edit your comment</h5>
+            </div><br>
+            
+            <h5>Comment: </h5>
+            <textarea type="text" name="body" class="form-control">{{$comment->body}}</textarea>
+            <br>
+            @error('body')
+                <div class="alert alert-danger" role="alert">
+                    <strong>{{ $message }}</strong>
+                </div>
+            @enderror
 
-        <input type="submit" value="Submit">
-        <a href = "{{ route('posts.show', ['category' => $comment->post->category,'post' => $comment->post ])}}">Cancel</a>
-    </form>
+            <div class="navbar">
+                <a href = "{{ route('posts.show', ['category' => $comment->post->category,'post' => $comment->post ])}}"><button type="button" class="btn">Cancel</button></a>
+                <input type="submit" value="Submit" class="btn">
+            </div>
+        </form>
+    </div>
 
 @endsection
