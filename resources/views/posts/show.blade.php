8d2fd7b35ae26d69112be97483ef54f331fd1faa
HamTart <hamtart@gmail.com>
2021-01-01 12:21:35 +0000

    Users can now edit their own comment on posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -87,15 +87,34 @@
                 <p>No Comments Yet!</p>
             @else
                 <div class="row">
-                    <div class="col">
-                        <div v-for="comment in comments" class="comment">  
-                            <p>@{{ comment.body }}</p>
-                            <div class="navbar">
-                                <div v-for="profile in profiles" v-if="profile.id === comment.id">
-                                    <p class="posted">Posted By: 
-                                        @{{profile.username}}</p>
+                    <div class="col" id="coms">
+                        <div v-for="comment in comments" class="comment"> 
+                            <div class="row">
+                                <div class="col">
+                                    <p>@{{ comment.body }}</p>
                                 </div>
-                                <p>@{{ comment.created_at }}</p>
+                                <div class="col">
+                                    <div v-for="profile in profiles" v-if="profile.id === comment.id">
+                                        <div v-if="profile.profile_id === <?php echo Auth::id(); ?>" id="edit-comm">
+                                            <a v-bind:href="getEditLink(comment.id)" class="edit-link">
+                                                <h5>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5L13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175l-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                                    </svg>
+                                                </h5> 
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                                
+                            <div class="row">
+                                <div class="col">
+                                    <div v-for="profile in profiles" v-if="profile.id === comment.id">
+                                        <p class="posted">Posted By: @{{profile.username}}</p>
+                                    </div>
+                                </div>
                             </div> 
                         </div>
                     </div>
@@ -148,6 +167,9 @@
                         .catch(response => {
                             console.log(response)
                         })
+                },
+                getEditLink: function(id){
+                    return 'https://posthub.test/comments/' + id + '/edit';
                 }
             }
         });
